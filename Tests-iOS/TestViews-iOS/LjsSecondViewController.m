b23654d9028af56d7cf49ea9820080065c733285
Joshua Moody <joshuajmoody@gmail.com>
2012-02-14 16:39:36 +0100

    glass and gradient views

--- Tests-iOS/TestViews-iOS/LjsSecondViewController.m
+++ Tests-iOS/TestViews-iOS/LjsSecondViewController.m
@@ -2,6 +2,10 @@
 #import "Lumberjack.h"
 #import "LoremIpsum.h"
 #import "LjsTextAndFont.h"
+#import "LjsGlassView.h"
+#import <QuartzCore/QuartzCore.h>
+#import "UIColor+LjsAdditions.h"
+#import "LjsGradientView.h"
 
 #ifdef LOG_CONFIGURATION_DEBUG
 static const int ddLogLevel = LOG_LEVEL_DEBUG;
@@ -64,7 +68,74 @@ static const int ddLogLevel = LOG_LEVEL_WARN;
   [self.view addSubview:tv];
   height = tv.contentSize.height;
   DDLogDebug(@"height of tv = %f", height);
+  
+  UIColor *highColor = [UIColor colorWithR:80 g:100 b:244];
+  UIColor *lowColor = [UIColor colorWithR:58 g:41 b:73];
+  
+  LjsGradientView *top = [[LjsGradientView alloc] initWithFrame:CGRectMake(0, 202, 320, 104)];
+  [top setHighColor:highColor lowColor:lowColor];
+  [top setBorderColor:[UIColor clearColor] borderWidth:0 cornerRadius:0];
+  [self.view addSubview:top];
+  
+  LjsGlassView *transport = [[LjsGlassView alloc] initWithFrame:CGRectMake(0, 308, 320, 103)];
+//  UIColor *highColor = [UIColor colorWithR:80 g:100 b:244];
+//  UIColor *lowColor = [UIColor colorWithR:58 g:41 b:73];
+//
+  //  CAGradientLayer *gradient = [CAGradientLayer layer];
+//  gradient.frame = transport.bounds;
+//  gradient.colors = [NSArray arrayWithObjects:(id)[highColor CGColor], (id)[lowColor CGColor], nil];
+//  [transport.layer insertSublayer:gradient atIndex:0];
+//  
+//  transport.backgroundColor = [UIColor blueColor];
+  [self.view addSubview:transport];
+  CGFloat y = (103/4) - (44/2);
+  CGFloat x = (320/2) - (44/2);
+  UIButton *middle = [[UIButton alloc] initWithFrame:CGRectMake(x, y, 44, 44)];
+  [middle setBackgroundColor:[UIColor whiteColor]];
+  [transport addSubview:middle];
 
+  x = ((320/4) * 3) - (44/2);
+  UIButton *right = [[UIButton alloc] initWithFrame:CGRectMake(x, y, 44, 44)];
+  [right setBackgroundColor:[UIColor whiteColor]];
+  [transport addSubview:right];
+  
+  x = (320/4) - (44/2);
+  UIButton *left = [[UIButton alloc] initWithFrame:CGRectMake(x, y, 44, 44)];
+  [left setBackgroundColor:[UIColor whiteColor]];
+  [transport addSubview:left];
+  
+  x = 20;
+  y = ((103/4) * 3) - (23 /2);
+  UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(x, y, 280, 23)];
+  [transport addSubview:slider];
+                
+  font = [UIFont systemFontOfSize:14];
+  UIColor *textColor = [UIColor whiteColor];
+  text = @"00:00:00";
+  CGSize labelSize = [text sizeWithFont:font];
+  CGFloat w = labelSize.width;
+  CGFloat h = labelSize.height;
+  x = 20;
+  //y = ((103/8) * 7) - (h / 2);
+  y = (103/8) * 7;
+  UILabel *leftLabel = [[UILabel alloc] initWithFrame:CGRectMake(x, y, w, h)];
+  leftLabel.backgroundColor = [UIColor clearColor];
+  leftLabel.text = text;
+  leftLabel.font = font;
+  leftLabel.textColor = textColor;
+  leftLabel.textAlignment = UITextAlignmentLeft;
+  [transport addSubview:leftLabel];
+  
+  x = 320 - (w + 20);
+  UILabel *rightLabel = [[UILabel alloc] initWithFrame:CGRectMake(x, y, w, h)];
+  rightLabel.backgroundColor = [UIColor clearColor];
+  rightLabel.text = text;
+  rightLabel.font = font;
+  rightLabel.textColor = textColor;
+  rightLabel.textAlignment = UITextAlignmentRight;
+  [transport addSubview:rightLabel];
+
+  
 }
 
 - (void)viewDidUnload
---
+++ Views-iOS/LjsGlassView.m
@@ -0,0 +1,73 @@
+// Copyright 2012 Little Joy Software. All rights reserved.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in
+//       the documentation and/or other materials provided with the
+//       distribution.
+//     * Neither the name of the Little Joy Software nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY LITTLE JOY SOFTWARE ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LITTLE JOY SOFTWARE BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "LjsGlassView.h"
+#import "Lumberjack.h"
+
+#ifdef LOG_CONFIGURATION_DEBUG
+static const int ddLogLevel = LOG_LEVEL_DEBUG;
+#else
+static const int ddLogLevel = LOG_LEVEL_WARN;
+#endif
+
+@implementation LjsGlassView
+
+
+#pragma mark Memory Management
+- (void) dealloc {
+   DDLogDebug(@"deallocating %@", [self class]);
+}
+
+- (void)drawRect:(CGRect)rect {
+  CGContextRef currentContext = UIGraphicsGetCurrentContext();
+  
+  CGGradientRef glossGradient;
+  CGColorSpaceRef rgbColorspace;
+  size_t num_locations = 2;
+  CGFloat locations[2] = { 0.0, 1.0 };
+  CGFloat components[8] = { 1.0, 1.0, 1.0, 0.35,  // Start color
+    1.0, 1.0, 1.0, 0.06 }; // End color
+  
+  rgbColorspace = CGColorSpaceCreateDeviceRGB();
+  glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
+  
+  CGRect currentBounds = self.bounds;
+  CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+  CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
+  CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
+  
+  CGGradientRelease(glossGradient);
+  CGColorSpaceRelease(rgbColorspace); 
+}
+
+
+@end
