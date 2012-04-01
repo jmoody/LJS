44df475be35033e179d563bccce1ed44e395df93
Joshua Moody <joshuajmoody@gmail.com>
2012-04-01 14:07:52 +0200

    added scales to location

--- Google/Models/Machine/_LjsGoogleLocation.h
+++ Google/Models/Machine/_LjsGoogleLocation.h
@@ -5,7 +5,19 @@
 
 
 extern const struct LjsGoogleLocationAttributes {
+	__unsafe_unretained NSString *latitude100km;
+	__unsafe_unretained NSString *latitude100m;
+	__unsafe_unretained NSString *latitude10km;
+	__unsafe_unretained NSString *latitude10m;
+	__unsafe_unretained NSString *latitude1km;
+	__unsafe_unretained NSString *latitude1m;
 	__unsafe_unretained NSString *latitudeNumber;
+	__unsafe_unretained NSString *longitude100km;
+	__unsafe_unretained NSString *longitude100m;
+	__unsafe_unretained NSString *longitude10km;
+	__unsafe_unretained NSString *longitude10m;
+	__unsafe_unretained NSString *longitude1km;
+	__unsafe_unretained NSString *longitude1m;
 	__unsafe_unretained NSString *longitudeNumber;
 	__unsafe_unretained NSString *type;
 } LjsGoogleLocationAttributes;
@@ -23,6 +35,18 @@ extern const struct LjsGoogleLocationFetchedProperties {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @interface LjsGoogleLocationID : NSManagedObjectID {}
 @end
 
@@ -35,6 +59,78 @@ extern const struct LjsGoogleLocationFetchedProperties {
 
 
 
+@property (nonatomic, strong) NSNumber *latitude100km;
+
+
+@property double latitude100kmValue;
+- (double)latitude100kmValue;
+- (void)setLatitude100kmValue:(double)value_;
+
+//- (BOOL)validateLatitude100km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *latitude100m;
+
+
+@property double latitude100mValue;
+- (double)latitude100mValue;
+- (void)setLatitude100mValue:(double)value_;
+
+//- (BOOL)validateLatitude100m:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *latitude10km;
+
+
+@property double latitude10kmValue;
+- (double)latitude10kmValue;
+- (void)setLatitude10kmValue:(double)value_;
+
+//- (BOOL)validateLatitude10km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *latitude10m;
+
+
+@property double latitude10mValue;
+- (double)latitude10mValue;
+- (void)setLatitude10mValue:(double)value_;
+
+//- (BOOL)validateLatitude10m:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *latitude1km;
+
+
+@property double latitude1kmValue;
+- (double)latitude1kmValue;
+- (void)setLatitude1kmValue:(double)value_;
+
+//- (BOOL)validateLatitude1km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *latitude1m;
+
+
+@property double latitude1mValue;
+- (double)latitude1mValue;
+- (void)setLatitude1mValue:(double)value_;
+
+//- (BOOL)validateLatitude1m:(id*)value_ error:(NSError**)error_;
+
+
+
+
 @property (nonatomic, strong) NSNumber *latitudeNumber;
 
 
@@ -43,6 +139,78 @@ extern const struct LjsGoogleLocationFetchedProperties {
 - (void)setLatitudeNumberValue:(double)value_;
 
 //- (BOOL)validateLatitudeNumber:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude100km;
+
+
+@property double longitude100kmValue;
+- (double)longitude100kmValue;
+- (void)setLongitude100kmValue:(double)value_;
+
+//- (BOOL)validateLongitude100km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude100m;
+
+
+@property double longitude100mValue;
+- (double)longitude100mValue;
+- (void)setLongitude100mValue:(double)value_;
+
+//- (BOOL)validateLongitude100m:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude10km;
+
+
+@property double longitude10kmValue;
+- (double)longitude10kmValue;
+- (void)setLongitude10kmValue:(double)value_;
+
+//- (BOOL)validateLongitude10km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude10m;
+
+
+@property double longitude10mValue;
+- (double)longitude10mValue;
+- (void)setLongitude10mValue:(double)value_;
+
+//- (BOOL)validateLongitude10m:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude1km;
+
+
+@property double longitude1kmValue;
+- (double)longitude1kmValue;
+- (void)setLongitude1kmValue:(double)value_;
+
+//- (BOOL)validateLongitude1km:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSNumber *longitude1m;
+
+
+@property double longitude1mValue;
+- (double)longitude1mValue;
+- (void)setLongitude1mValue:(double)value_;
+
+//- (BOOL)validateLongitude1m:(id*)value_ error:(NSError**)error_;
 
 
 
@@ -85,11 +253,119 @@ extern const struct LjsGoogleLocationFetchedProperties {
 @interface _LjsGoogleLocation (CoreDataGeneratedPrimitiveAccessors)
 
 
+- (NSNumber *)primitiveLatitude100km;
+- (void)setPrimitiveLatitude100km:(NSNumber *)value;
+
+- (double)primitiveLatitude100kmValue;
+- (void)setPrimitiveLatitude100kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLatitude100m;
+- (void)setPrimitiveLatitude100m:(NSNumber *)value;
+
+- (double)primitiveLatitude100mValue;
+- (void)setPrimitiveLatitude100mValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLatitude10km;
+- (void)setPrimitiveLatitude10km:(NSNumber *)value;
+
+- (double)primitiveLatitude10kmValue;
+- (void)setPrimitiveLatitude10kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLatitude10m;
+- (void)setPrimitiveLatitude10m:(NSNumber *)value;
+
+- (double)primitiveLatitude10mValue;
+- (void)setPrimitiveLatitude10mValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLatitude1km;
+- (void)setPrimitiveLatitude1km:(NSNumber *)value;
+
+- (double)primitiveLatitude1kmValue;
+- (void)setPrimitiveLatitude1kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLatitude1m;
+- (void)setPrimitiveLatitude1m:(NSNumber *)value;
+
+- (double)primitiveLatitude1mValue;
+- (void)setPrimitiveLatitude1mValue:(double)value_;
+
+
+
+
 - (NSNumber *)primitiveLatitudeNumber;
 - (void)setPrimitiveLatitudeNumber:(NSNumber *)value;
 
 - (double)primitiveLatitudeNumberValue;
 - (void)setPrimitiveLatitudeNumberValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude100km;
+- (void)setPrimitiveLongitude100km:(NSNumber *)value;
+
+- (double)primitiveLongitude100kmValue;
+- (void)setPrimitiveLongitude100kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude100m;
+- (void)setPrimitiveLongitude100m:(NSNumber *)value;
+
+- (double)primitiveLongitude100mValue;
+- (void)setPrimitiveLongitude100mValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude10km;
+- (void)setPrimitiveLongitude10km:(NSNumber *)value;
+
+- (double)primitiveLongitude10kmValue;
+- (void)setPrimitiveLongitude10kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude10m;
+- (void)setPrimitiveLongitude10m:(NSNumber *)value;
+
+- (double)primitiveLongitude10mValue;
+- (void)setPrimitiveLongitude10mValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude1km;
+- (void)setPrimitiveLongitude1km:(NSNumber *)value;
+
+- (double)primitiveLongitude1kmValue;
+- (void)setPrimitiveLongitude1kmValue:(double)value_;
+
+
+
+
+- (NSNumber *)primitiveLongitude1m;
+- (void)setPrimitiveLongitude1m:(NSNumber *)value;
+
+- (double)primitiveLongitude1mValue;
+- (void)setPrimitiveLongitude1mValue:(double)value_;
 
 
 
--- Google/Models/Machine/_LjsGoogleLocation.m
+++ Google/Models/Machine/_LjsGoogleLocation.m
@@ -4,7 +4,19 @@
 #import "_LjsGoogleLocation.h"
 
 const struct LjsGoogleLocationAttributes LjsGoogleLocationAttributes = {
+	.latitude100km = @"latitude100km",
+	.latitude100m = @"latitude100m",
+	.latitude10km = @"latitude10km",
+	.latitude10m = @"latitude10m",
+	.latitude1km = @"latitude1km",
+	.latitude1m = @"latitude1m",
 	.latitudeNumber = @"latitudeNumber",
+	.longitude100km = @"longitude100km",
+	.longitude100m = @"longitude100m",
+	.longitude10km = @"longitude10km",
+	.longitude10m = @"longitude10m",
+	.longitude1km = @"longitude1km",
+	.longitude1m = @"longitude1m",
 	.longitudeNumber = @"longitudeNumber",
 	.type = @"type",
 };
@@ -42,8 +54,56 @@ const struct LjsGoogleLocationFetchedProperties LjsGoogleLocationFetchedProperti
 + (NSSet *)keyPathsForValuesAffectingValueForKey:(NSString *)key {
 	NSSet *keyPaths = [super keyPathsForValuesAffectingValueForKey:key];
 	
+	if ([key isEqualToString:@"latitude100kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude100km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"latitude100mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude100m"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"latitude10kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude10km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"latitude10mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude10m"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"latitude1kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude1km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"latitude1mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"latitude1m"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
 	if ([key isEqualToString:@"latitudeNumberValue"]) {
 		NSSet *affectingKey = [NSSet setWithObject:@"latitudeNumber"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude100kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude100km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude100mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude100m"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude10kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude10km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude10mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude10m"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude1kmValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude1km"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"longitude1mValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"longitude1m"];
 		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
 	}
 	if ([key isEqualToString:@"longitudeNumberValue"]) {
@@ -53,6 +113,162 @@ const struct LjsGoogleLocationFetchedProperties LjsGoogleLocationFetchedProperti
 
 	return keyPaths;
 }
+
+
+
+
+@dynamic latitude100km;
+
+
+
+- (double)latitude100kmValue {
+	NSNumber *result = [self latitude100km];
+	return [result doubleValue];
+}
+
+- (void)setLatitude100kmValue:(double)value_ {
+	[self setLatitude100km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude100kmValue {
+	NSNumber *result = [self primitiveLatitude100km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude100kmValue:(double)value_ {
+	[self setPrimitiveLatitude100km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic latitude100m;
+
+
+
+- (double)latitude100mValue {
+	NSNumber *result = [self latitude100m];
+	return [result doubleValue];
+}
+
+- (void)setLatitude100mValue:(double)value_ {
+	[self setLatitude100m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude100mValue {
+	NSNumber *result = [self primitiveLatitude100m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude100mValue:(double)value_ {
+	[self setPrimitiveLatitude100m:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic latitude10km;
+
+
+
+- (double)latitude10kmValue {
+	NSNumber *result = [self latitude10km];
+	return [result doubleValue];
+}
+
+- (void)setLatitude10kmValue:(double)value_ {
+	[self setLatitude10km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude10kmValue {
+	NSNumber *result = [self primitiveLatitude10km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude10kmValue:(double)value_ {
+	[self setPrimitiveLatitude10km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic latitude10m;
+
+
+
+- (double)latitude10mValue {
+	NSNumber *result = [self latitude10m];
+	return [result doubleValue];
+}
+
+- (void)setLatitude10mValue:(double)value_ {
+	[self setLatitude10m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude10mValue {
+	NSNumber *result = [self primitiveLatitude10m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude10mValue:(double)value_ {
+	[self setPrimitiveLatitude10m:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic latitude1km;
+
+
+
+- (double)latitude1kmValue {
+	NSNumber *result = [self latitude1km];
+	return [result doubleValue];
+}
+
+- (void)setLatitude1kmValue:(double)value_ {
+	[self setLatitude1km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude1kmValue {
+	NSNumber *result = [self primitiveLatitude1km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude1kmValue:(double)value_ {
+	[self setPrimitiveLatitude1km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic latitude1m;
+
+
+
+- (double)latitude1mValue {
+	NSNumber *result = [self latitude1m];
+	return [result doubleValue];
+}
+
+- (void)setLatitude1mValue:(double)value_ {
+	[self setLatitude1m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLatitude1mValue {
+	NSNumber *result = [self primitiveLatitude1m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLatitude1mValue:(double)value_ {
+	[self setPrimitiveLatitude1m:[NSNumber numberWithDouble:value_]];
+}
+
 
 
 
@@ -77,6 +293,162 @@ const struct LjsGoogleLocationFetchedProperties LjsGoogleLocationFetchedProperti
 
 - (void)setPrimitiveLatitudeNumberValue:(double)value_ {
 	[self setPrimitiveLatitudeNumber:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude100km;
+
+
+
+- (double)longitude100kmValue {
+	NSNumber *result = [self longitude100km];
+	return [result doubleValue];
+}
+
+- (void)setLongitude100kmValue:(double)value_ {
+	[self setLongitude100km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude100kmValue {
+	NSNumber *result = [self primitiveLongitude100km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude100kmValue:(double)value_ {
+	[self setPrimitiveLongitude100km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude100m;
+
+
+
+- (double)longitude100mValue {
+	NSNumber *result = [self longitude100m];
+	return [result doubleValue];
+}
+
+- (void)setLongitude100mValue:(double)value_ {
+	[self setLongitude100m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude100mValue {
+	NSNumber *result = [self primitiveLongitude100m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude100mValue:(double)value_ {
+	[self setPrimitiveLongitude100m:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude10km;
+
+
+
+- (double)longitude10kmValue {
+	NSNumber *result = [self longitude10km];
+	return [result doubleValue];
+}
+
+- (void)setLongitude10kmValue:(double)value_ {
+	[self setLongitude10km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude10kmValue {
+	NSNumber *result = [self primitiveLongitude10km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude10kmValue:(double)value_ {
+	[self setPrimitiveLongitude10km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude10m;
+
+
+
+- (double)longitude10mValue {
+	NSNumber *result = [self longitude10m];
+	return [result doubleValue];
+}
+
+- (void)setLongitude10mValue:(double)value_ {
+	[self setLongitude10m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude10mValue {
+	NSNumber *result = [self primitiveLongitude10m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude10mValue:(double)value_ {
+	[self setPrimitiveLongitude10m:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude1km;
+
+
+
+- (double)longitude1kmValue {
+	NSNumber *result = [self longitude1km];
+	return [result doubleValue];
+}
+
+- (void)setLongitude1kmValue:(double)value_ {
+	[self setLongitude1km:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude1kmValue {
+	NSNumber *result = [self primitiveLongitude1km];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude1kmValue:(double)value_ {
+	[self setPrimitiveLongitude1km:[NSNumber numberWithDouble:value_]];
+}
+
+
+
+
+
+@dynamic longitude1m;
+
+
+
+- (double)longitude1mValue {
+	NSNumber *result = [self longitude1m];
+	return [result doubleValue];
+}
+
+- (void)setLongitude1mValue:(double)value_ {
+	[self setLongitude1m:[NSNumber numberWithDouble:value_]];
+}
+
+- (double)primitiveLongitude1mValue {
+	NSNumber *result = [self primitiveLongitude1m];
+	return [result doubleValue];
+}
+
+- (void)setPrimitiveLongitude1mValue:(double)value_ {
+	[self setPrimitiveLongitude1m:[NSNumber numberWithDouble:value_]];
 }
 
 
